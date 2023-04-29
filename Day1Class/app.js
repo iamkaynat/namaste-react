41a4ee08b96088ba9ffc12e1e51669844b3053d6
Unzala <unzkay196@gmail.com>
2023-04-29 14:58:44 +0530

    episode 1

---
+++ Day1Class/app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement("div", { id: "parent" }, React.createElement("div", { id: "child" }, React.createElement("h1", { id: "heading" }, "Hello World!")));
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
